0f8a7a830fdbd7085fd902be41358939bd8424cc
Thiago Terleski <thiagoterleski@gmail.com>
2017-07-31 07:54:06 -0300

    Delete config.js

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-export const marvelApi = {
-  publicKey: '90477e77a3b01a078ec22b190e677c21',
-  privateKey: 'c31db2a8d4c56f22f86dd437f66da424f96b8591',
-  baseUrl: `${window.location.protocol || 'http'}//gateway.marvel.com:80`,
-}
